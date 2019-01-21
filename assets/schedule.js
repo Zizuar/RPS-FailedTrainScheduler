dad419e850be7891dd4a8147fd3faa8c9266cc29
Donald P Polansky <zizuar@comcast.net>
2019-01-20 21:12:06 -0600

    Struggling with moment.js. Need a save point.

--- assets/schedule.js
+++ assets/schedule.js
@@ -16,31 +16,61 @@ var config = {
  var idi;
  var interval;
  var etaCal;
- 
+ var nShuttleTime;
+
+ var idiConverted = moment(idi, "HH:mm").subtract(1, "years");
+    console.log("idi: " + idiConverted);
+ var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+var diffTime = moment().diff(moment(idiConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+var etaCal = diffTime % interval;
+    console.log(etaCal);
+
+
+
+var tMinutesTillTrain = interval - etaCal;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    
+var nShuttleTime = moment().add(tMinutesTillTrain, "minutes");
+// var nShuttleTime = Date.now().add(tMinutesTillTrain, "minutes");
+// var nShuttleTime = Date.now() ++ (tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nShuttleTime).format("hh:mm"));
+    
  $("#submitButton").on("click", function () {
     shuttle = $("#shuttle").val().trim();
     dport = $("#dport").val().trim();
-    idi = $("#idi").val().trim();
+    idi = moment($("#idi").val().trim(), "HH:mm").format("X");
     interval = $("#interval").val().trim();
+    // etaCal = $("#etaCal").val().trim();
+    
+
  
- 
+
     database.ref().push({
         "shuttle": shuttle,
         "dport": dport,
         "idi": idi,
         "interval": interval
+        
+        // "etaCal": etaCal
     })
+
+    $("#train-input").val("");
+	$("#destination-input").val("");
+	$("#time-input").val("");
+	$("#frequency-input").val("");
  
  })
 
+
+
+
+
  database.ref().on("child_added", function (childSnapshot) {
-    console.log(childSnapshot)
-    console.log(childSnapshot.val().shuttle);
-    console.log(childSnapshot.val().dport);
-    console.log(childSnapshot.val().idi);
-    console.log(childSnapshot.val().interval);
- 
-    var newShuttle = $("<tr><th>"+childSnapshot.val().shuttle+"</th><th>"+ childSnapshot.val().dport +"</th><th>"+ childSnapshot.val().interval +"</th><th>"+ childSnapshot.val().interval +"</th></tr>");
+     var newShuttle = $("<tr><th>"+childSnapshot.val().shuttle+"</th><td>"+ childSnapshot.val().dport +"</td><td>"+ childSnapshot.val().interval +"</td><td>"+ childSnapshot.val().interval +"</td></tr>");
     $("#shuttleTable").append(newShuttle);
  
  });
